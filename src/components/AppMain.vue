721edbeddc973d3ffa8760e1f0ac995e554ce600
karfaisarra <karfaisarra@gmail.com>
2022-11-16 15:37:08 +0100

    add commit

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -21,17 +21,59 @@ export default {
         axios(config)
             .then(response => {
                 console.log(response.data);
-                this.state.characters = response.data
+                this.state.characters = response.data.results
             })
             .catch(error => {
                 console.log(error);
                 this.state.error = error.message
             })
-    }
+    },
+    /*methods: {
+        searchByName() {
+            const searchName = this.state.searchName
+            const newUrl = `https://api.themoviedb.org/3/discover/movie?api_key=e48d16052578167c382756ce83bc5c4a&query=tomb raider&with_title=${searchName}`
+            console.log(newUrl);
+            axios.get(newUrl)
+                .then(response => {
+                    this.state.characters = response.data
+
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+    }*/
+
 }
 </script>
 <template>
     <main>
+        <div class="container py-5">
+            <div class="searchbar">
+                <input type="text" v-model="state.searchName" @keyup="searchByName">
+                <button>Search</button>
+            </div>
+            <div class="movies mt-5">
+                <div class="movie " v-for="(character, i) in state.characters" v-show="state.searchName !== ''">
+                    <ul>
+                        <li>
+                            <p>
+                                <strong>{{ i + 1 }}) Titolo:</strong> {{ character.title }}
+                            </p>
+                            <p>
+                                <strong>Titolo Originale:</strong> {{ character.original_title }}
+                            </p>
+                            <p>
+                                <strong>Lingua:</strong> {{ character.original_language }}
+                            </p>
+                            <p>
+                                <strong>Voto:</strong> {{ character.vote_average }}
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
--- src/store.js
+++ src/store.js
@@ -2,4 +2,6 @@ import { reactive } from 'vue'
 export const store = reactive({
     characters: null,
     error: null,
+    url: 'https://api.themoviedb.org/3/movie/550?api_key=e48d16052578167c382756ce83bc5c4a&query=tomb%20raider',
+    searchName: '',
 })
