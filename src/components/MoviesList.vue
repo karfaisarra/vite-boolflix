48f59a5e405b786c80dff09c057fafbee3808941
karfaisarra <karfaisarra@gmail.com>
2022-11-17 15:25:37 +0100

    add changes

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -12,24 +12,22 @@ export default {
 <template>
     <div class="movies mt-5">
         <div class="movie " v-for="(movie, i) in state.movies">
-            <ul v-for="serie in state.series">
+            <ul>
                 <li>
                     <div class="titolo">
-                        <strong>{{ i + 1 }}) Titolo:</strong> {{ movie.title }} / {{ serie.name }}
+                        <strong>{{ i + 1 }}) Titolo:</strong> {{ movie.title }}
                     </div>
                     <div class="titolo_originale">
-                        <strong>Titolo Originale:</strong> {{ movie.original_title }} / {{ serie.original_name }}
+                        <strong>Titolo Originale:</strong> {{ movie.original_title }}
                     </div>
                     <div class="lingua" v-for="bandiera in state.bandiere"
                         v-show="movie.original_language == bandiera.language">
 
-                        <strong>Lingua:</strong> <img width="30" :src="bandiera.flag" alt=""> /{{
-                                serie.original_language
-                        }}
+                        <strong>Lingua:</strong> <img width="30" :src="bandiera.flag" alt="">
                     </div>
 
                     <div class="voto">
-                        <strong>Voto:</strong> {{ movie.vote_average }}/{{ serie.vote_average }}
+                        <strong>Voto:</strong> {{ movie.vote_average }}
                     </div>
                 </li>
             </ul>
