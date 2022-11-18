52b3c4549f60ab2991ccaff189313448edbdb937
karfaisarra <karfaisarra@gmail.com>
2022-11-18 16:47:35 +0100

    add flags

--- src/state.js
+++ src/state.js
@@ -1,54 +1,17 @@
 import { reactive } from 'vue'
 import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 export const state = reactive({
+
     allResults: {
         movies: null,
         tv: null
     },
     error: null,
-    bandiere: [
-        {
-            language: 'fr',
-            flag: 'http://www.geonames.org/flags/x/fr.gif'
-        },
-        {
-            language: 'it',
-            flag: 'http://www.geonames.org/flags/x/it.gif'
-        },
-        {
-            language: 'ja',
-            flag: 'http://www.geonames.org/flags/x/jp.gif'
-        },
-        {
-            language: 'en',
-            flag: 'http://www.geonames.org/flags/x/us.gif'
-        },
-        {
-            language: 'ar',
-            flag: 'http://www.geonames.org/flags/x/sa.gif'
-        },
-        {
-            language: 'nl',
-            flag: 'http://www.geonames.org/flags/x/nl.gif'
-        },
-        {
-            language: 'de',
-            flag: 'http://www.geonames.org/flags/x/de.gif'
-        },
-        {
-            language: 'pl',
-            flag: 'http://www.geonames.org/flags/x/pl.gif'
-        },
-        {
-            language: 'tr',
-            flag: 'http://www.geonames.org/flags/x/tr.gif'
-        },
-        {
-            language: 'hi',
-            flag: 'http://www.geonames.org/flags/x/in.gif'
-        }
-
-    ],
+    flags: ['fr', 'it', 'en', 'ar', 'ja', 'nl', 'de', 'tr', 'pl', 'hi', 'zh'],
     params: {
         api_key: 'e48d16052578167c382756ce83bc5c4a',
         query: ''
